e7c44b5aab50ca3dd3582866f1c0bc2e6e6fd16a
YuyaIsh <dame.blazer@gmail.com>
2023-11-05 23:30:32 +0900

    整理

--- create_bat.py
+++ create_bat.py
@@ -30,20 +30,25 @@ def main():
     envs = [f for f in files if os.path.isdir(os.path.join(envs_path, f))]
 
     # pythonファイルを取得してリスト化
-    efficiency_folder_path = r"D:\data\programming\efficiency"
-    python_folder_path = rf"{efficiency_folder_path}\python"
-    python_files = os.listdir(python_folder_path)
-    python_files = [python_file for python_file in python_files if python_file[-3:]==".py"]
-
+    efficiency_folder_path = r"E:\programming\efficiency"
+    app_files = {}
+    for i in os.listdir(efficiency_folder_path):
+        if os.path.isdir(os.path.join(efficiency_folder_path,i)):
+            for j in os.listdir(os.path.join(efficiency_folder_path,i)):
+                if os.path.splitext(j)[1]==".py" and "env" not in j:
+                    app_files[j] = i
+    print(app_files)
     layout = [
-        [sg.Text("Create bat",font=fontsize(22,True),pad=(2,(2,10)))],
+        [sg.Text("Crea te bat",font=fontsize(22,True),pad=(2,(2,10)))],
         [sg.Column([
             [sg.Text("Enviroment")],
             [sg.Combo(envs,default_value="bat",size=10,key="env")]
         ])],
         [sg.Column([
-            [sg.Text("Python file")],
-            [sg.Combo(python_files,default_value=python_files[0],size=20,key="python_file")]
+            [sg.Text("App file")],
+            [sg.Combo(list(app_files.keys()),
+                      default_value=list(app_files.keys())[0],
+                      size=20,key="app_file")]
         ])],
         [sg.Column([
             [sg.Checkbox("Create vbs file",key="create_vbs",default=True)]
@@ -51,14 +56,16 @@ def main():
         [sg.Column([
             [sg.Text("",key="result"),sg.Button("Create",key="create_file")],
         ],justification="right")],
-        ]
+    ]
 
 
-    window = sg.Window("Create bat",
-                       layout,
-                       location=location,
-                       finalize=True,
-                       font=fontsize(18))
+    window = sg.Window(
+        "Create bat",
+        layout,
+        location=location,
+        finalize=True,
+        font=fontsize(18)
+    )
 
 #################################################################
 ############################ process ############################
@@ -69,19 +76,20 @@ def main():
         if event == None:
             break
 
-        if event == "create_file":
+        elif event == "create_file":
             env = values["env"]
-            python_file = values["python_file"]
+            app_file = values["app_file"]
+            parent_folder = app_files[app_file]
 
-            if env and python_file:
+            if env and app_file:
                 bat_content = fr'''
 @chcp 65001
 @call "E:\Miniconda\condabin\activate.bat" {env}
-@{envs_path}\{env}\python.exe "{python_folder_path}\{python_file}" %*
+@{envs_path}\{env}\python.exe "{efficiency_folder_path}\{parent_folder}\{app_file}" %*
 '''
-                python_file_name = python_file[:-3]
+                python_file_name = os.path.splitext(app_file)[0]
 
-                bat_file_path = rf"{efficiency_folder_path}\bat\{python_file_name}.bat"
+                bat_file_path = os.path.join(efficiency_folder_path,"bat",f"{python_file_name}.bat")
                 try:
                     with open(bat_file_path,"w",encoding='utf-8') as f:
                         f.write(bat_content)
@@ -98,7 +106,7 @@ oShell.run "{python_file_name}.bat", 0
 Set oShell = Nothing
 '''
 
-                vbs_file_path = rf"{efficiency_folder_path}\bat\{python_file_name}.vbs"
+                vbs_file_path = os.path.join(efficiency_folder_path,"bat",f"{python_file_name}.vbs")
                 if values["create_vbs"]:
                     try:
                         with open(vbs_file_path,"w",encoding='utf-8') as f:
@@ -112,7 +120,7 @@ Set oShell = Nothing
 
             elif env == "":
                 sg.PopupError(f"Environment field is blank.",font=fontsize(18),location=(600,400),keep_on_top=True)
-            elif python_file == "":
+            elif app_file == "":
                 sg.PopupError(f"Python file field is blank.",font=fontsize(18),location=(600,400),keep_on_top=True)
 
 
